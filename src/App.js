f287850649a4a8ab425b61bb6373b9a2ee1468c5
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2025-11-18 19:32:56 +0530

    modified user-profile.js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
-    <Router>
-      {/* <Router basename={process.env.REACT_APP_BASENAME}> */}
+    // <Router>
+      <Router basename={process.env.REACT_APP_BASENAME}>
       <ToastContainer
         position="top-right"
         autoClose={3000}
